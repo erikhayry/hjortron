b0944e1cdece527381876ae09024710652af472b
Erik Portin <erikportin@gmail.com>
2013-09-12 20:23:24 +0100

    updated karma config

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,55 +1,62 @@
-// Karma E2E configuration
+module.exports = function(config) {
+    config.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+		// Karma E2E configuration
 
-// list of files / patterns to load in the browser
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/**/*.js'
-];
+		// base path, that will be used to resolve files and exclude
+		basePath : '',
 
-// list of files to exclude
-exclude = [];
+		frameworks : ["ng-scenario"],	
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress', 'growl'];
+		// list of files / patterns to load in the browser
+		files : [
+		  //ANGULAR_SCENARIO,
+		  //ANGULAR_SCENARIO_ADAPTER,
+		  'test/e2e/**/*.js'
+		],
 
-// web server port
-port = 9876;
+		// list of files to exclude
+		exclude : [],
 
-// cli runner port
-runnerPort = 9100;
+		// test results reporter to use
+		// possible values: dots || progress || growl
+		reporters : ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+		// web server port
+		port : 9876,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_DEBUG;
+		// cli runner port
+		runnerPort : 9100,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+		// enable / disable colors in the output (reporters and logs)
+		colors : true,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel : config.LOG_DISABLE,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch : false,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers : ['Chrome'],
 
-proxies = { '/': 'http://localhost:9000' };
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout : 5000,
 
-urlRoot = '/__karma/';
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun : true,
+
+		proxies : { '/': 'http://localhost:9000' },
+
+		urlRoot : '/__karma/'
+	});
+}
